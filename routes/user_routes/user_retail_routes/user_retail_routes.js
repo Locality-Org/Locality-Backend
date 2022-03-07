923c74b2fe6b50d73234ce626cc57e22fa02e0b5
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2022-03-07 13:24:28 +0530

    feat: create get_subCategory route and api

--- routes/user_routes/user_retail_routes/user_retail_routes.js
+++ routes/user_routes/user_retail_routes/user_retail_routes.js
@@ -36,3 +36,31 @@ router.route('/get_category/:catId').get(async (req, res) => {
 		return res.status(200).json(_response);
 	} catch (error) {}
 });
+
+router.route('get_subCategory/:catId').get(async (req, res) => {
+	try {
+		const _subcategory = await Category.exists({ id: req.params.catId });
+		if (!_subcategory) {
+			return res.status(404).json({
+				message: 'No subcategory was found!',
+			});
+		}
+		const _ads = await Ad.find({
+			$and: [
+				{
+					section: 'Category',
+				},
+				{
+					parent: req.params.catId,
+				},
+			],
+		});
+		const _response = {
+			// TODO: Implement after shop and store API is complete
+			popular_products: [],
+			products: [],
+			ads: _ads.map((_ad) => ({ media: _ad.media })),
+		};
+		return res.status(200).json(_response);
+	} catch (error) {}
+});
